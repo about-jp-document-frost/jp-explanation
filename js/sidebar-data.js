04d556ec049f3abb1efee53caf9deb3c6a53c481
Kenfrost <kentacho3@gmail.com>
2026-04-12 16:41:03 +0900

    add: some topics

--- js/sidebar-data.js
+++ js/sidebar-data.js
@@ -29,7 +29,8 @@ window.SIDEBAR_CONFIG = {
         ["document/education/index.html", "Index"],
         ["document/education/english-education-japan.html", "English Education"],
         ["document/education/general-subjects.html", "General Subjects"],
-        ["document/education/multiple-language-education.html", "Multiple Language"]
+        ["document/education/multiple-language-education.html", "Multiple Language"],
+        ["document/education/discussion-is-hated.html", "Discussion Culture"]
       ]
     },
     {
